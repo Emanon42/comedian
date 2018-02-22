966e3456cc4840be40604f5f0c0062c5909fc647
Yueyang Tang <EmanonTang@outlook.com>
2018-02-22 16:48:47 +0000

    take the image

--- tester.py
+++ tester.py
@@ -14,12 +14,18 @@ def show_webcam():
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         print(len(faces))
-        for (x,y,w,h) in faces:
-            cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),2)
+        for index,(x,y,w,h) in enumerate(faces):
+
+            if cv2.waitKey(1) == 32:
+                face = img[x:x+w,y:y+h]
+                faceName = "face"+str(index)+".png"
+                cv2.imwrite(faceName,face)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         cv2.imshow('my webcam', img)
         if cv2.waitKey(1) == 27:
             break
+
     cv2.destroyAllWindows()
 
 def main():
