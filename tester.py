e466bd407bd1c5bfccad0995423ad2620dfb6e29
Yueyang Tang <EmanonTang@outlook.com>
2018-02-22 16:38:43 +0000

    rectangle drawing on face

--- tester.py
+++ tester.py
@@ -3,12 +3,19 @@ import cv2
 
 def show_webcam():
     cam = cv2.VideoCapture(0)
-    #cam.set(3, 1280)
-    #cam.set(4, 720)
+    width = int(((cam.get(cv2.CAP_PROP_FRAME_WIDTH))/2)-200)
+    height = int(((cam.get(cv2.CAP_PROP_FRAME_HEIGHT))/2)-200)
+    face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
     while True:
 
         ret_val, img = cam.read()
-        img = img[0:400,0:400]
+
+        #img = img[width:width+400,height:height+400]
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        print(len(faces))
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),2)
 
         cv2.imshow('my webcam', img)
         if cv2.waitKey(1) == 27:
