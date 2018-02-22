77c5ecb23623ec7f53f39518c7a0340f74697be2
Yueyang Tang <EmanonTang@outlook.com>
2018-02-22 14:00:10 +0000

    resize

--- tester.py
+++ tester.py
@@ -3,10 +3,13 @@ import cv2
 
 def show_webcam():
     cam = cv2.VideoCapture(0)
-
+    #cam.set(3, 1280)
+    #cam.set(4, 720)
     while True:
+
         ret_val, img = cam.read()
-        #res = img[0:640,0:640]
+        img = img[0:400,0:400]
+
         cv2.imshow('my webcam', img)
         if cv2.waitKey(1) == 27:
             break
