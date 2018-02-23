05081f76e180299f4b4fe801c8d159fc7cdfd7b6
Yueyang Tang <EmanonTang@outlook.com>
2018-02-23 12:22:30 +0000

    fix grey

--- tester.py
+++ tester.py
@@ -1,5 +1,6 @@
 import cv2
 from lib import detect
+import time
 
 
 def show_webcam():
@@ -13,19 +14,21 @@ def show_webcam():
 
         #img = img[width:width+400,height:height+400]
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         faces_ = detect.SmileDetector("lib/weights/detection_weights.pth","lib/weights/classification_weights.pth")
+        t0 = time.time()
         detecteds = faces_.detect_faces(img)
+        print(time.time()-t0)
         print(len(detecteds))
         #print(len(faces))
-        for index,(x,y,w,h) in enumerate(faces):
-
-            if cv2.waitKey(1) == 32:
-                face = img[y:y+h,x:x+w]
-                face = cv2.resize(face,(64,64))
-                faceName = "face"+str(index)+".png"
-                cv2.imwrite(faceName,face)
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # for index,(x,y,w,h) in enumerate(faces):
+        #
+        #     if cv2.waitKey(1) == 32:
+        #         face = img[y:y+h,x:x+w]
+        #         face = cv2.resize(face,(64,64))
+        #         faceName = "face"+str(index)+".png"
+        #         cv2.imwrite(faceName,face)
+        #     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         cv2.imshow('my webcam', img)
         if cv2.waitKey(1) == 27:
