0a77c81c2fb176ae648cb684e34a24007e1d35f1
Johannes <j.laute3@googlemail.com>
2018-02-23 13:07:46 +0000

    should now work asynchronously

--- lib/tasks.py
+++ lib/tasks.py
@@ -1,14 +1,11 @@
 from celery import Celery
+import celery
 from .detect import SmileDetector
 
 app = Celery("tasks", broker="pyamqp://guest@localhost//")
 
-class Smile(app.Task):
-    def __init__(self):
-        self.detector = SmileDetector("lib/weights/detection_weights.pth","lib/weights/classification_weights.pth")
+detector = SmileDetector("lib/weights/detection_weights.pth","lib/weights/classification_weights.pth")
 
-    def run(self, image):
-        return self.detector.call(image)
-
-
-detectTask = app.tasks[Smile.name]
+@app.tasks
+def detect(image):
+    return detector.call(image)
